2b2a08a53e476a0135c1cb1dd8a7d39c7e142aae
seungrokj <seungrokj>
2026-01-05 11:38:57 +0000

    atom mi300x
    Signed-off-by: seungrokj <seungrokj>

--- runners/launch_mi300x-amdatom.sh
+++ runners/launch_mi300x-amdatom.sh
@@ -15,7 +15,7 @@
 # HF_TOKEN
 # FRAMEWORK
 
-HF_HUB_CACHE_MOUNT="/mnt/hf_hub_cache/"  # Temp solution
+HF_HUB_CACHE_MOUNT="/home/amd/models"  # Temp solution
 PORT=8888
 
 # Determine framework suffix for benchmark script
