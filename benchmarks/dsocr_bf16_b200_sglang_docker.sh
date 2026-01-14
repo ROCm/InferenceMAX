0fe756be3772be41341e9d1be930a85056ffe66a
seungrokjung <seungrok.jung@amd.com>
2026-01-14 07:57:48 +0000

    sglang
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- benchmarks/dsocr_bf16_b200_sglang_docker.sh
+++ benchmarks/dsocr_bf16_b200_sglang_docker.sh
@@ -21,6 +21,9 @@ nvidia-smi
 
 SERVER_LOG=$(mktemp /tmp/server-XXXXXX.log)
 
+#dsocr requires addict
+pip install addict
+
 set -x
 python3 -m sglang.launch_server --model-path $MODEL --host 0.0.0.0 --port $PORT --trust-remote-code \
 --tensor-parallel-size=$TP --data-parallel-size=1 \
