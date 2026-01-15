cb53e621cafce582e5e02b2a4d2312056f91546c
seungrokjung <seungrok.jung@amd.com>
2026-01-15 04:03:14 -0600

    fix
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- benchmarks/dsocr_bf16_mi355x_sglang_docker.sh
+++ benchmarks/dsocr_bf16_mi355x_sglang_docker.sh
@@ -14,6 +14,9 @@
 
 SERVER_LOG=$(mktemp /tmp/server-XXXXXX.log)
 
+#dsocr requires 
+pip install addict easydict
+
 set -x
 python3 -m sglang.launch_server --model-path $MODEL --host 0.0.0.0 --port $PORT --trust-remote-code \
 --tensor-parallel-size=$TP --data-parallel-size=1 \
