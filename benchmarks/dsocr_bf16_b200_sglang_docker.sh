feb9a81bf0d8f135b54be08857e4e3e06ed9a7dd
seungrokjung <seungrok.jung@amd.com>
2026-01-16 02:28:26 +0000

    sgl b200
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- benchmarks/dsocr_bf16_b200_sglang_docker.sh
+++ benchmarks/dsocr_bf16_b200_sglang_docker.sh
@@ -23,6 +23,7 @@ SERVER_LOG=$(mktemp /tmp/server-XXXXXX.log)
 
 #dsocr requires 
 pip install addict easydict
+pip install -U transformers
 
 set -x
 python3 -m sglang.launch_server --model-path $MODEL --host 0.0.0.0 --port $PORT --trust-remote-code \
